7da338abda299ab0c8395623c2847e8056e0e276
Zephyros1938.org <xtrau505true@gmail.com>
2024-10-10 15:45:37 +0000

    Chat Update

--- src/main.js
+++ src/main.js
@@ -1,47 +1,61 @@
 const serverconfig = require('./serverconfig.json')
+const utilities = require('./utilities')
 
-const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
+const path = require('path')
+const fs = require('fs')
 const ip = require('ip')
-const path = require('node:path')
 
-const app = express()
-const server = http.createServer(app);
-const io = new socketIo.Server(server);
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+    const url = req.url == '/' ? "/index.html" : req.url
+    var p;
+    try {
+        p = fs.readFileSync(path.join(__dirname, 'public', url))
+        res.writeHead(200);
+    } catch {
+        p = Buffer.alloc(1, 0)
+        res.writeHead(404);
+    }
 
-app.get('*', (req, res) => {
-    dir = (__dirname+ '/public'+ req.path)
-    console.log(dir)
-    res.sendFile(dir)
-})
-
-const coords = {}
-
-io.on("connection", (socket) => {
-    ipaddr = socket.handshake.address;
-
-    console.log('A user connected');
-    socket.emit('message', "hello")
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-
-    socket.on('coords push', (x) => {coords[[ipaddr]] = x})
+    res.end(p);
 });
 
-if (serverconfig.autoIP) {
-    serverconfig.address = ip.address()
-}
+// Create a Socket.IO server
+const io = new Server(server);
 
-setInterval(() => {
-    if (Object.keys(coords).length>0){
-        console.log(coords)
-        io.sockets.emit('universal coords', coords)
-    }
-}, 20);
+const positions = {}
 
-server.listen({ address: serverconfig.address, port: serverconfig.port }, () => {
-    console.log(`Server is running at http://${serverconfig.address}:${serverconfig.port}`);
+// Listen for connection events
+io.on('connection', (socket) => {
+});
+
+const io_texts = io.of('/texts')
+io_texts.on("connection", (socket) => {
+    const ipaddr = socket.handshake.address
+
+    socket.emit("your name", ipaddr)
+
+    io_texts.emit('add user', ipaddr)
+    io_texts.emit("position update", positions)
+
+    socket.on("chat post", (name, text) => {
+        io_texts.emit("chat emit", (`${name.padEnd(15," ")} : ${utilities.escapeHtml(text)}\n`))
+    })
+
+    socket.on("disconnect", () => {
+        io_texts.emit("remove user", ipaddr)
+        delete positions[[ipaddr]]
+    })
+    socket.on('position push', (data) => {
+        positions[[ipaddr]] = { x: data[0], y: data[1] }
+        io_texts.emit("position update", positions)
+    })
+})
+
+
+serverconfig.address = serverconfig.autoIP == true ? ip.address() : serverconfig.address;
+server.listen({ host: serverconfig.address, port: serverconfig.port }, () => {
+    console.log(`Server is running on http://${serverconfig.address}:${serverconfig.port}`);
 });
